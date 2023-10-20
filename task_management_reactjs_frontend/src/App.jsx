cfe57ef452699f50b5867ec77aeaee2e018eb49f
SanketSarmalkar <sanket.sarmalkar2002@gmail.com>
2023-10-20 11:31:45 +0530

    feat/initial_commit-frontend

---
+++ task_management_reactjs_frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import {TasksPage} from './pages/TasksPage'
+
+export default function App(){
+  return (
+    <div>
+      <TasksPage/>
+    </div>
+  )
+}
---
+++ task_management_reactjs_frontend/src/pages/TasksPage.jsx
@@ -0,0 +1,7 @@
+export default function TasksPage(){
+    return (
+        <div>
+        <h1 style={{color: "#202020"}}>Task</h1>Tasks Page
+        </div>
+    )
+}
